03439474989ef799701d246a750dc6c66e7d4343
Armin Braun <me@obrown.io>
2018-08-30 08:01:09 +0200

    Finish implementing integration tests

--- src/test/java/com/oz/segmentio/avro/IntegrationTests.java
+++ src/test/java/com/oz/segmentio/avro/IntegrationTests.java
@@ -1,11 +1,19 @@
 package com.oz.segmentio.avro;
 
+import com.google.common.io.Resources;
 import com.oz.segmentio.json.JsonToAvroUtils;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
+import org.apache.avro.Schema;
 import org.apache.avro.specific.SpecificRecordBase;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static com.oz.segmentio.avro.FullCycleTests.assertFullCycle;
 import static org.junit.Assert.fail;
 
 public final class IntegrationTests {
@@ -19,14 +27,29 @@ public final class IntegrationTests {
                     "If you are running these tests from an IDE please invoke `mvn clean test` instead to rebuild AVRO schemata."
             );
         }
-        Collection<String> testCases = new ArrayList<>();
-        schemata.stream().filter(cls -> {
-            try {
-
-                return true;
-            } catch (Exception ignored) {
-                return false;
-            }
-        });
+        // Add aggregate Tracking event at the end of the classes to test so it's used as a fallback should no specific class match
+        schemata.add(Tracking.class);
+        try (DirectoryStream<Path> directoryStream =
+                 Files.newDirectoryStream(
+                     Paths.get(Resources.getResource(IntegrationTests.class, "").toURI())
+                 )
+        ) {
+            directoryStream.forEach(file -> {
+                if (file.toString().endsWith(".json")) {
+                    Assertions.assertThat(schemata.stream().filter(cls -> {
+                        try {
+                            assertFullCycle(
+                                file.getFileName().toString().replaceAll(".json$", ""),
+                                (Schema) cls.getMethod("getClassSchema", null).invoke(null),
+                                cls
+                            );
+                            return true;
+                        } catch (Exception ignored) {
+                            return false;
+                        }
+                    }).findFirst()).isPresent();
+                }
+            });
+        }
     }
 }
